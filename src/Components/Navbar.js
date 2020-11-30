b69b7f7762098f7118ef57c1f43b45752d699af1
Laika-Beats <laikabeatsdenver@gmail.com>
2020-11-29 19:50:23 -0700

    navbar sticky effect

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,18 +1,38 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import "./navbar.scss"
 
+
+
 const Navbar = () => {
+    const [scrolled, setScrolled] = React.useState(false)
+    const handleScroll = () => {
+        const offset = window.scrollY
+        if (offset > 200) {
+            setScrolled(true)
+        } else {
+            setScrolled(false)
+        }
+    }
+
+    useEffect( () => {
+        window.addEventListener("scroll", handleScroll)
+    })
+
+    let x = ["navbar"]
+    if (scrolled) {
+        x.push("scrolled")
+    }
     return (
-        <header className = {navbarClasses.join(" ")}>
+        <header className = {x.join(" ")}>
             <div>
                 <p>Joshua Elliot Thompson</p>
             </div>
             <nav className="navigation">
                 <ul>
-                    <li><a>About</a></li>
-                    <li><a>Portfolio</a></li>
-                    <li><a>Connect</a></li>
-                    <li><a>Contact</a></li>
+                    <li><a href="">About</a></li>
+                    <li><a href="">Portfolio</a></li>
+                    <li><a href="">Socials</a></li>
+                    <li><a href="">Contact</a></li>
                 </ul>
             </nav>
         </header>
