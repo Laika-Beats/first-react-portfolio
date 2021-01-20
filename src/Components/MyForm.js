d93da0573afd95987478660c5f43529365345854
Laika-Beats <laikabeatsdenver@gmail.com>
2021-01-19 21:48:35 -0700

    edit

--- src/Components/MyForm.js
+++ src/Components/MyForm.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 
 export const Container = styled.div`
   min-height: 692px;
+
   position: fixed;
   bottom: 0;
   left: 0;
@@ -11,6 +12,7 @@ export const Container = styled.div`
   top: 0;
   z-index: 0;
   overflow: hidden;
+
   background: linear-gradient(
     108deg,
     rgba(1, 147, 86, 1) 0%,
@@ -18,6 +20,7 @@ export const Container = styled.div`
   );
 `;
 export const FormWrap = styled.div`
+  margin-top: 55px;
   height: 100%;
   display: flex;
   flex-direction: column;
